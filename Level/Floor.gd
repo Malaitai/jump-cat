8e3e5873adeeb9ccf2bfd59de8e64a5b3d6abc93
Malaitai <christian@asdah.school.nz>
2021-11-17 20:19:44 +1300

    Fixed obstacle

---
+++ Level/Floor.gd
@@ -0,0 +1,6 @@
+extends StaticBody2D
+
+const  OBSTACLE = preload("res://Obstacles/Obstacle.tscn")
+
+func _on_Timer_timeout(): # connected to the floor
+	pass
--- Obstacles/Obstacle.gd
+++ Obstacles/Obstacle.gd
@@ -1,4 +1,4 @@
 extends Area2D
 
-func _process(delta):
-	position.x -= 10
+func _process(delta): # delta is the distance between the current frame and the previous frame
+	position.x -= 500 * delta
