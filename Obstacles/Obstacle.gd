1cd952f39dafdc0c092b7c965c7e7f32892aa8c1
Malaitai <christian@asdah.school.nz>
2021-11-17 20:48:49 +1300

    Modified files

--- Obstacles/Obstacle.gd
+++ Obstacles/Obstacle.gd
@@ -2,3 +2,11 @@ extends Area2D
 
 func _process(delta): # delta is the distance between the current frame and the previous frame
 	position.x -= 500 * delta
+
+
+func _on_VisibilityNotifier2D_screen_exited(): # when the obstacle leaves the screen (level screen)
+	queue_free()
+
+
+func _on_Obstacle_body_shape_entered(body_rid, body, body_shape_index, local_shape_index): # end game when the obstacle collides with the player
+	get_tree().quit()
--- Player/Player.gd
+++ Player/Player.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 const GRAVITY = 10
-const JUMP_SPEED = 400
+const JUMP_SPEED = -400
 var velocity = Vector2.ZERO # (0,0)
 
 # INBUILT FUNCTIONS FOR GODOT
@@ -12,7 +12,7 @@ func _process(delta):
 	velocity.y += GRAVITY
 	
 	if Input.is_action_just_pressed("jump") and is_on_floor(): # when the user presses the UP button AND is_on_floor, do the following
-		velocity.y =- JUMP_SPEED
+		velocity.y = JUMP_SPEED
 	
 	move_and_slide(velocity, Vector2.UP) # linear velocity in one direction, direction (pointing up in this case)
 	
